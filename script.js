0a4c42e54a1818b54ccd834299f5156ba53f0674
TKWarrior7623 <00001110312520sp@al.educacao.sp.gov.br>
2024-09-25 00:09:00 -0300

    Versão 1.1
    Atualização de algumas mudanças

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Leonardo Takeshiro Souza Toyama 3D 22/09/2024
 
-const avanca = document.querySelectorAll('.btn-proximo');
+const avanca = document.querySelectorAll('.btn-proximo'); //criação da constante "avanca" 
 
 avanca.forEach(button => {
     button.addEventListener('click', function(){
@@ -10,4 +10,4 @@ avanca.forEach(button => {
         atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
     })
-}) //Atribuindo função para os botões, dando constantes de ativo, atual e proximo passo
+}) //Atribuindo função para os botões, utilizando meios para ativar e desativar as telas
